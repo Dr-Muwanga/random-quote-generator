0800d2d3524442c9693bdfb45b18097735953da8
David Muwanga <dmuwanga25@gmail.com>
2023-09-25 21:36:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Container from './components/container'
+
+function App() {
+ 
+
+  return (
+    <main id='wrapper'>
+        <Container />
+    </main>
+  )
+}
+
+export default App
